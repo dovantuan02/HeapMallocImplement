c3b2b63cd1306513f344e7fe546935a34ea315e0
dovantuan02 <dovantuan285@gmail.com>
2024-09-27 13:55:43 +0700

    first commit

---
+++ Malloc/config.h
@@ -0,0 +1,60 @@
+#ifndef _heap_5_h
+#define _heap_5_h
+
+#include "stdio.h"
+#include "stdint.h"
+#include "assert.h"
+
+#define configTOTAL_HEAP_SIZE       4096
+
+#define configSUPPORT_DYNAMIC_ALLOCATION 1
+#define portBYTE_ALIGNMENT       1
+
+#define portMAX_DELAY    ( TickType_t ) 0xffffffffffffffffU
+#define portPOINTER_SIZE_TYPE    size_t
+
+#if portBYTE_ALIGNMENT == 32
+    #define portBYTE_ALIGNMENT_MASK    ( 0x001f )
+#elif portBYTE_ALIGNMENT == 16
+    #define portBYTE_ALIGNMENT_MASK    ( 0x000f )
+#elif portBYTE_ALIGNMENT == 8
+    #define portBYTE_ALIGNMENT_MASK    ( 0x0007 )
+#elif portBYTE_ALIGNMENT == 4
+    #define portBYTE_ALIGNMENT_MASK    ( 0x0003 )
+#elif portBYTE_ALIGNMENT == 2
+    #define portBYTE_ALIGNMENT_MASK    ( 0x0001 )
+#elif portBYTE_ALIGNMENT == 1
+    #define portBYTE_ALIGNMENT_MASK    ( 0x0000 )
+#else /* if portBYTE_ALIGNMENT == 32 */
+    #error "Invalid portBYTE_ALIGNMENT definition"
+#endif /* if portBYTE_ALIGNMENT == 32 */
+
+
+#define PRIVILEGED_FUNCTION
+#define PRIVILEGED_DATA
+#define FREERTOS_SYSTEM_CALL
+
+#define FATAL(fmt, ...)     printf(fmt, "FATAL - ", ##__VA_ARGS__)
+
+#define configASSERT(x)     assert((x))
+
+
+#ifndef traceMALLOC
+    #define traceMALLOC( pvAddress, uiSize )
+#endif
+
+#ifndef traceFREE
+    #define traceFREE( pvAddress, uiSize )
+#endif
+
+#ifndef mtCOVERAGE_TEST_MARKER
+    #define mtCOVERAGE_TEST_MARKER()
+#endif
+
+
+extern void * pvPortMalloc( size_t xWantedSize );
+extern void * pvPortCalloc( size_t xNum, size_t xSize );
+extern void vPortFree( void * pv );
+extern size_t xPortGetFreeHeapSize( void );
+extern void vPortHeapResetState( void );
+#endif
